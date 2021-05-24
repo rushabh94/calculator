fc3b2bdf02c2f6d13db7a2865aecee6ca76bb03f
rushab9450h <rushabhnavapara.pingdr@gmail.com>
2021-05-24 09:51:19 +0530

    four commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -29,97 +29,97 @@ function Calculator() {
 
           {/* gird */}
 
-          <div class='grid-container'>
-            <div class='grid-item item1'>
+          <div className='grid-container'>
+            <div className='grid-item item1'>
               <button onClick={backspace} id='backspace'>
                 C
               </button>
             </div>
-            <div class='grid-item item2'>
+            <div className='grid-item item2'>
               <button onClick={clear} id='clear'>
                 Clear
               </button>
               {/* Clear */}
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='7' onClick={handleClick}>
                 7
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='8' onClick={handleClick}>
                 8
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='9' onClick={handleClick}>
                 9
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='/' onClick={handleClick}>
                 &divide;
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='4' onClick={handleClick}>
                 4
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='5' onClick={handleClick}>
                 5
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='6' onClick={handleClick}>
                 6
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='*' onClick={handleClick}>
                 &times;
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='1' onClick={handleClick}>
                 1
               </button>
             </div>
 
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='2' onClick={handleClick}>
                 2
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='3' onClick={handleClick}>
                 3
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='+' onClick={handleClick}>
                 +
               </button>
             </div>
 
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='.' onClick={handleClick}>
                 .
               </button>
             </div>
 
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='0' onClick={handleClick}>
                 0
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button onClick={total} id='result'>
                 =
               </button>
             </div>
-            <div class='grid-item'>
+            <div className='grid-item'>
               <button name='-' onClick={handleClick}>
                 &ndash;
               </button>
